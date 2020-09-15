04fde432ed2372a12e9a0c1029da0be6adc4b1a4
hoshikimu <you@example.com>
2020-09-15 23:22:07 +0900

    Finish 2-21

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 'use strict';
 
-function showAd() {
+function showAd(message = 'Ad') {
   console.log('========');
-  console.log('===Ad===');
+  console.log(`===${message}===`);
   console.log('========');
 }
 
+showAd('Header Ad');
+console.log('A');
+console.log('A');
+console.log('A');
+console.log('A');
+// showAd('Ad');
 showAd();
 console.log('A');
 console.log('A');
 console.log('A');
 console.log('A');
-showAd();
-console.log('A');
-console.log('A');
-console.log('A');
-console.log('A');
+showAd('Footer Ad');
