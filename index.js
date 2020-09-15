3c9a974ddd85db5b877054057de737ca8552ae70
hoshikimu <you@example.com>
2020-09-15 19:16:38 +0900

    Finish 2-19

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 'use strict';
 
-// let hp = 100;
-
-// while (hp > 0) {
-//   console.log(`${hp} HP left`);
-//   hp -= 15;
-// }
-
-let hp = 15;
-
-do {
-  console.log(`${hp} HP left`);
-  hp -= 15;
-} while (hp > 0);
+for (let i = 1; i <= 10; i++) {
+  if (i % 3 === 0) {
+    continue;
+  }
+  console.log(i);
+}
