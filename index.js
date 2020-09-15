f73657389632385ad4cdb92bdbb43c7652692a41
hoshikimu <you@example.com>
2020-09-15 14:16:30 +0900

    Finish 2-9

--- index.js
+++ index.js
@@ -3,7 +3,11 @@
 // console.log(150 * 140);
 // console.log(150 * 160);
 
-const price = 150;
+let price = 500;
 
-console.log(price * 140);
-console.log(price * 160);
+price += 100; //600
+price *= 2; 
+price++;
+price--;
+
+console.log(price);
