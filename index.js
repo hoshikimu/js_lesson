6bfa91478704868c9b24a8c980ad9a2c2cf85ac5
hoshikimu <you@example.com>
2020-09-15 13:36:57 +0900

    Finish 2-5

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
 'use strict';
 
-const num = 5;
-const winner = Math.floor(Math.random() * num); //0 - 4
-
-for(let i = 0; i < num; i++) {
-  const div = document.createElement('div');
-  div.classList.add('box');
-
-  div.addEventListener('click', () => {
-    if (i === winner) {
-      div.textContent = 'Win!';
-      div. classList.add('win');
-    } else {
-      div.textContent = 'Lose!';
-      div. classList.add('lose');
-    }
-  });
-
-  document.body.appendChild(div);
-}
+console.log("it's me!");
+console.log('it\'s me!');
+console.log('Hel\nlo wo\trld')
