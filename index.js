807e4015624c8115410e27b4e54a9c11fd99feaf
hoshikimu <you@example.com>
2020-09-16 18:08:19 +0900

    Finish 3-18

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 'use strict';
 
 {
-  const str = 'hello';
+  const d = [2019, 1, 12];
+  console.log(d.join('/'))
 
-  // console.log(str.length);
-  console.log(str.substring(2, 4));
-  console.log(str[1]);
+  const t = '17:08:24'
+  // console.log(t.split(':'));
+  const [hour, minute, second] = t.split(':');
+  console.log(hour);
+  console.log(minute);
+  console.log(second);
+
 }
