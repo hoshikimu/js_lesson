e6f274b627dfaf141fe0bcf3835afe1579395a79
hoshikimu <you@example.com>
2020-09-16 21:21:53 +0900

    Finish 3-23

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
 'use strict';
 
 {
-  const scores = [10, 3, 9];
-  let sum = 0;
-
-  scores.forEach(score => {
-    sum += score;
-  });
-
-
-  const avg = sum / scores.length;
-
-  // console.log(sum);
-  // console.log(avg);
-
-  console.log(Math.floor(avg));
-  console.log(Math.ceil(avg));
-  console.log(Math.round(avg));
-  console.log(avg.toFixed(3));
-
-  console.log(Math.random());
-
+  // alert('hello');
+  const answer = confirm('削除しますか？');
+  if (answer) {
+    console.log('削除しました');
+  } else {
+    console.log('キャンセルしました');
+  }
 }
