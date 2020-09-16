f7496255d8843c929f4e206505ea5759012d2d47
hoshikimu <you@example.com>
2020-09-16 18:00:03 +0900

    Finish 3-17

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 'use strict';
 
 {
-  let x = [1,  2];
-  let y = [...x];
-  x[0] = 5;
-  console.log(x);
-  console.log(y);
+  const str = 'hello';
+
+  // console.log(str.length);
+  console.log(str.substring(2, 4));
+  console.log(str[1]);
 }
