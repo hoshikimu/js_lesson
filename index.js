0369027d33f3955961fa9d0d95b1c66e70a688c1
hoshikimu <you@example.com>
2020-09-16 15:09:16 +0900

    Finish 3-12

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 'use strict';
 
 {
-  const point = {x: 100, y: 180};
+  const point = {
+    x: 100,
+    y: 180,
+  };
+
+  point.x = 120;
+  // console.log(point.x);
+  // console.log(point['y']);
+  point.z = 90;
+  delete point.y;
   console.log(point);
 }
