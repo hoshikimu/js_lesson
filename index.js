8f50b3a995176f06890050a5c5d79a221e697941
hoshikimu <you@example.com>
2020-09-16 13:05:29 +0900

    Finish 3-2

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 'use strict';
 
 {
-//   const score = 80;
-//   const score = 90;
-//   const score = 40;
-
   const scores = [80, 90, 40];
-  console.log(scores);
+  // console.log(scores[1]);
+
+  // scores[2] = 44;
+  // console.log(scores);
+
+  console.log(scores.length);
 }
