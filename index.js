aafb1c7d5d4b794f2c8cc0965b2d574e56aa5bab
hoshikimu <you@example.com>
2020-09-17 23:32:17 +0900

    Finish 4-2

--- index.js
+++ index.js
@@ -1,27 +1,11 @@
 'use strict';
 
 {
-  class Post {
-    constructor(text) {
-      this.text = text;
-      this.likeCount = 0;
-    }
-
-    show() {
-      console.log(`${this.text} - ${this.likeCount}いいね`);
-    }
-
-    static showInfo() {
-      console.log('Post class version 1.0');
-    }
+  function update() {
+    // document.querySelector('h1').textContent = 'Changed';
+    // document.querySelector('#target').tectContent = 'Changed';
+    document.getElementById('target').textContent = 'Changed';
   }
 
-  const posts = [
-    new Post('aaaa'),
-    new Post('bbbb'),
-  ];
-
-  // posts[0].show();
-  // posts[1].show();
-  Post.showInfo();
+  setTimeout(update, 1000);
 }
