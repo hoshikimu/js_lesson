8199947e6b79fd5bbdcee7a70dfe62d726d347e2
hoshikimu <h.k.fir.m.ice.hs@gmail.com>
2020-10-01 20:02:21 +0900

    Finish 6-7

--- index.js
+++ index.js
@@ -7,17 +7,21 @@
       source[i] = i + 1 + 15 * col;
     }
 
-    const b = [];
+    const column = [];
     for (let i = 0; i < 5; i++) {
-      b[i] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
+      column[i] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
     }
-    console.log(b);
+    
+    return column;
   }
 
   const columns = []
   columns[0] = createColunm(0);
-  columns[1] = createColunm(0);
-  columns[2] = createColunm(0);
-  columns[3] = createColunm(0);
-  columns[4] = createColunm(0);
+  columns[1] = createColunm(1);
+  columns[2] = createColunm(2);
+  columns[3] = createColunm(3);
+  columns[4] = createColunm(4);
+  columns[2][2] = 'FREE';
+
+  console.table(columns);
 }
