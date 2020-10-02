a5f1b1d83be023e57456731e536bccdcc08caabf
hoshikimu <h.k.fir.m.ice.hs@gmail.com>
2020-10-03 00:18:53 +0900

    Finish 7-9

--- index.js
+++ index.js
@@ -5,6 +5,22 @@ console.clear();
   const year = 2020;
   const month = 4;
 
+  function getCalendarHead() {
+    const dates = [];
+    const d = new Date(year, month, 0).getDate();
+    const n = new Date(year, month, 1).getDay();
+
+    for (let i = 0; i < n; i++) {
+      dates.unshift({
+        date: d - i,
+        isToday: false,
+        isDisabled: true,
+      });
+    }
+
+    console.log(dates);
+  }
+
   function getCalendarBody() {
     const dates = [];
     const lastDate = new Date(year, month + 1, 0).getDate();
@@ -20,5 +36,5 @@ console.clear();
     console.log(dates);
   }
 
-  getCalendarBody();
+  getCalendarHead();
 }
