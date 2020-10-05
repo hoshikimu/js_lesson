1cee3fac8800036587056000d23af74500dd29ed
hoshikimu <h.k.fir.m.ice.hs@gmail.com>
2020-10-05 19:20:34 +0900

    Finish 9-8

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 'use strict';
 
 {
+  function setWord() {
+    word = words[Math.floor(Math.random() * words.length)];
+    target.textContent = word;
+    loc = 0;
+  }
+
   const words = [
   'red',
   'blue',
@@ -8,10 +14,9 @@
   ];
   let word = "red";
   let loc = 0;
-
   const target = document.getElementById('target');
-  word = words[Math.floor(Math.random() * words.length)];
-  target.textContent = word;
+  
+  setWord();
 
   document.addEventListener('keydown', e => {
     if (e.key !== word[loc]) {
@@ -20,6 +25,10 @@
     
     loc++;
     target.textContent = '_'.repeat(loc) + word.substring(loc);
+
+    if (loc === word.length) {
+      setWord();
+    }
 
   });
 }
