3d06322532d57eaa044fc5fe00e939ac66c9d97d
hoshikimu <h.k.fir.m.ice.hs@gmail.com>
2020-10-09 17:46:21 +0900

    Finish 13-8

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 'use strict';
 
 {
+  class Panel {
+    constructor() {
+      this.el = document.createElement('li');
+      this.el.classList.add('pressed');
+    }
+
+    getEl() {
+      return this.el;
+    }
+  }
+
   class Board {
     constructor() {
-      this.panels = []:
+      this.panels = [];
       for (let i = 0; i < 4; i++) {
         this.panels.push(new Panel());
       }
+      this.setUp()
+    }
+
+    setUp() {
+      const board = document.getElementById('board');
+      this.panels.forEach(panel => {
+        board.appendChild(panel.getEl());
+      });
     }
   }
 
