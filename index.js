c3bfc4b6816fe47a8e5e9ed410405a9730a82efa
hoshikimu <h.k.fir.m.ice.hs@gmail.com>
2020-10-09 17:08:24 +0900

    Finish 13-7

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 'use strict';
 
 {
-  
+  class Board {
+    constructor() {
+      this.panels = []:
+      for (let i = 0; i < 4; i++) {
+        this.panels.push(new Panel());
+      }
+    }
+  }
+
+  const board = new Board();
 }
